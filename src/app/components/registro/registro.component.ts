ed60de5e268d42ec81c70dc65ed8dd699c5423b8
edgardo90 <edgardo9000@gmail.com>
2025-11-25 02:54:16 -0300

    conectar registro al backEnd de aws

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl, ValidatorFn } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { IProfessionalData } from '../../interfaces/IProfessional'
 
 @Component({
   selector: 'app-registro',
@@ -11,27 +14,22 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractContro
 })
 export class RegistroComponent {
   form!: FormGroup; // 🔹 Se declara, pero no se inicializa aún
-  specialties = ['Veterinaria', 'Peluqueria'];
+  specialties = ['Veterinaria', 'Peluqueria', 'veterinario y peluqueria'];
+  isLoading = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router,) {
     this.form = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50), this.noNumbersValidator()]],
-    lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50), this.noNumbersValidator()]],
-    dni: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(10), this.onlyNumbersValidator()]],
-    phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(15), this.onlyNumbersValidator()]],
-    license: ['', [Validators.required]],
-    specialty: ['', [Validators.required]],
-    nameEstablishment: ['', [Validators.required]],
-    street: ['', [Validators.required]],
-    streetNumber: ['', [Validators.required]],
-    neighborhood: ['', [Validators.required]],
-    province: ['', [Validators.required]],
-    postalCode: ['', [Validators.required]],
-    location: ['', [Validators.required]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(8)]],
-    confirmPassword: ['', [Validators.required]]
-  }, { validators: this.passwordsMatchValidator() });
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50), this.noNumbersValidator()]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50), this.noNumbersValidator()]],
+      dni: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(10), this.onlyNumbersValidator()]],
+      phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(15), this.onlyNumbersValidator()]],
+      license: ['', [Validators.required]],
+      specialty: ['', [Validators.required]],
+      nameEstablishment: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validators: this.passwordsMatchValidator() });
 
   }
 
@@ -77,16 +75,45 @@ export class RegistroComponent {
   }
 
   submit() {
+    this.isLoading = true
     if (this.form.invalid) {
       this.form.markAllAsTouched();
+      this.isLoading = false
       return;
     }
-
-    const payload = { ...this.form.value };
-    delete (payload as any).confirmPassword;
-
+    const payload: IProfessionalData = {
+      name: this.form.value.name,
+      lastname: this.form.value.lastname,
+      dni: this.form.value.dni,
+      phone: this.form.value.phone,
+      email: this.form.value.email,
+      password: this.form.value.password,
+      nameEstablishment: this.form.value.nameEstablishment,
+      license: this.form.value.license,
+      specialty: this.form.value.specialty
+    };
     console.log('payload de registro (listo para enviar):', payload);
+    this.authService.register(payload).subscribe({
+      next: (res) => {
+        // console.log(res)
+        alert('Registro exitoso , espere el correo electronio a su email');
+        this.router.navigate(['/'])
+      },
+      error: (err) => {
+        const { error } = err
+        // console.log(error)
+        if (error.name === 'SequelizeUniqueConstraintError' && error?.errors?.length > 0) {
+          alert("Error en los datos , verifica el dni y email")
+        }
+        alert("Error al registrar , vuelva a intentarlo")
+        this.isLoading = false
+      },
+      complete: () => {
+        this.isLoading = false
+      }
+    })
   }
+
   onKeyPressLettersOnly(event: KeyboardEvent) {
     const char = event.key;
     const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]*$/;
